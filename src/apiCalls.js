70b4a180a1a3ddc68cf3edf133aabbb601c030ed
Daniel O'Connell <danieloconnell.maker@gmail.com>
2022-02-06 13:00:31 -0800

    Add api calls

---
+++ src/apiCalls.js
@@ -0,0 +1,11 @@
+const getSingleMovie = (id) => {
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+  .then(response => response.json())
+}
+
+const getAllMovies = () => {
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies`)
+  .then(response => response.json())
+}
+
+export {getSingleMovie, getAllMovies}
