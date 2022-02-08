ded1d7ffb73272842157f81dc126333d3cf270af
Jani Palomino <palomino.jani@gmail.com>
2022-02-08 03:07:53 -0700

    Add and implement new fonts

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -11,7 +11,7 @@ const Card = (props) => {
     <article className='movie-wrapper' onClick={ () => props.selectMovie(props.id) }>
       <img className='thumbnail' src={props.poster_path} alt={`"${props.title}" poster`}/>
       <div className='description-container'>
-        <h4>{props.title}</h4>
+        <h2>{props.title}</h2>
         <div className='rating-container'>
           <ReactStars className='star-rating' count={5} value={averageRating} size={16} color2={'#ffd700'} color1={'#0D0D0D'} edit={false} />
           <p>{props.average_rating}</p>
