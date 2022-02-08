21d29962a84002225ce01d92720ff992b257f30a
Jani Palomino <palomino.jani@gmail.com>
2022-02-08 00:43:49 -0700

    Add react stars package to movie cards

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactStars from 'react-stars'
 import './Card.scss'
 
 const Card = (props) => {
@@ -9,7 +10,10 @@ const Card = (props) => {
       <img className='thumbnail' src={props.poster_path} alt={`"${props.title}" poster`}/>
       <div className='description-container'>
         <h4>{props.title}</h4>
-        <p>Rating: {props.average_rating} / 5</p>
+        <div className='rating-container'>
+          <ReactStars className='star-rating' count={5} value={props.average_rating} size={16} color={'#f2f2f2'} color1={'#020202'} />
+          <p>{props.average_rating}</p>
+        </div>
       </div>
     </article>
   )
