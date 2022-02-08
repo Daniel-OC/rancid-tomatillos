1d6b890d1044e302f9c89474914a6e2718943a9f
Jani Palomino <palomino.jani@gmail.com>
2022-02-08 01:41:57 -0700

    Convert rating string to a number

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -5,13 +5,15 @@ import './Card.scss';
 const Card = (props) => {
   // const [show, setShow] = React.useState(false);
   // console.log(props)
+  const averageRating = Number(props.average_rating);
+
   return (
-    <article className='movie-wrapper' onClick ={ () => props.selectMovie(props.id) }>
+    <article className='movie-wrapper' onClick={ () => props.selectMovie(props.id) }>
       <img className='thumbnail' src={props.poster_path} alt={`"${props.title}" poster`}/>
       <div className='description-container'>
         <h4>{props.title}</h4>
         <div className='rating-container'>
-          <ReactStars className='star-rating' count={5} value={props.average_rating} size={16} color2={'#ffd700'} color1={'#0D0D0D'} edit={false} />
+          <ReactStars className='star-rating' count={5} value={averageRating} size={16} color2={'#ffd700'} color1={'#0D0D0D'} edit={false} />
           <p>{props.average_rating}</p>
         </div>
       </div>
