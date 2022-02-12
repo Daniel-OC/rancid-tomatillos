14ec24e827fdb8ec2288e98fd90d2fbc68b6ffae
Daniel O'Connell <danieloconnell.maker@gmail.com>
2022-02-12 15:30:14 -0800

    Stub out custom data so testing works regardless of API

--- cypress/integration/footer_spec.js
+++ cypress/integration/footer_spec.js
@@ -4,6 +4,19 @@ describe('Footer flows', () => {
   });
 
   it('The Footer should have all expected elements', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/',
+    {
+      statusCode: 200,
+      ok: true,
+      body: {movies:[{
+        "id": 694919,
+        "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+        "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+        "title": "Money Plane",
+        "average_rating": 6.666666666666667,
+        "release_date": "2020-09-29"
+      }]}
+    })
     cy.visit('http://localhost:3000')
     cy.get('Footer')
       .get('.footer-content').get('.project-description').get('h3').contains('About').get('p')
--- cypress/integration/movieContainer_spec.js
+++ cypress/integration/movieContainer_spec.js
@@ -4,18 +4,31 @@ describe('MovieContainer User Flows', () => {
   });
 
   it('MovieContainer should have all expected elements', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/',
+    {
+      statusCode: 200,
+      ok: true,
+      body: {movies:[{
+        "id": 694919,
+        "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+        "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+        "title": "Money Plane",
+        "average_rating": 6.666666666666667,
+        "release_date": "2020-09-29"
+      }]}
+    })
     cy.visit('http://localhost:3000')
     cy.get('.movie-container').get('.movie-wrapper').get('.thumbnail')
       .get('.description-container').get('h2').get('.rating-container').get('.star-rating' && 'p').get('.info-button').contains('More Info')
   });
 
-  it('MovieContainer should have all expected elements', () => {
-    cy.intercept('GET','https://rancid-tomatillos.herokuapp.com/api/v2/movies/',
-      {
-        statusCode: 404,
-        ok: false,
-      }
-    )
-    cy.visit('http://localhost:3000')
-  })
+  // it('MovieContainer should display an error in the event of a 404 error', () => {
+  //   cy.intercept('GET','https://rancid-tomatillos.herokuapp.com/api/v2/movies/',
+  //     {
+  //       statusCode: 404,
+  //       ok: false,
+  //     }
+  //   )
+  //   cy.visit('http://localhost:3000')
+  // })
 })
